e96248241e91ff67268ce504e66dd4103c46c345
ravi4rails <ravionrails@gmail.com>
2016-04-21 00:48:09 +0530

    heroku fix.

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
-before_filter :configure_sign_up_params, only: [:create]
-before_filter :configure_account_update_params, only: [:update]
+# before_filter :configure_sign_up_params, only: [:create]
+# before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,15 +38,15 @@ before_filter :configure_account_update_params, only: [:update]
 
   # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << :first_name, :last_name
-  end
+  # # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.for(:sign_up) << :first_name, :last_name
+  # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) << :first_name, :last_name
-  end
+  # # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.for(:account_update) << :first_name, :last_name
+  # end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
